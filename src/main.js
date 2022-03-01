93af5ab4893939ff228cde78c9118dad9b596ad6
tofu0924 <in_no@naver.com>
2022-03-01 22:01:13 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false
 
